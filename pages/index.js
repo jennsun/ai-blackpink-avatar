d25532259b25b42cf8b86662f43552dd604e66b0
Jenny <jennysunnjm@gmail.com>
2023-01-16 21:30:04 -0800

    Calling the Interference API

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,139 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState, useEffect } from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+import twitterLogo from '../assets/twitter-logo.webp'
 
 const Home = () => {
+  // Create state property
+  const maxRetries = 20
+  const [input, setInput] = useState('')
+  const [img, setImg] = useState('')
+
+  // Numbers of retries
+  const [retry, setRetry] = useState(0)
+  // Number of retries left
+  const [retryCount, setRetryCount] = useState(maxRetries)
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  // Add this function
+  const onChange = (event) => {
+    setInput(event.target.value)
+  }
+
+  const generateAction = async () => {
+    console.log('Generating...')
+
+    if (isGenerating && retry === 0) return
+
+    setIsGenerating(true)
+
+    if (retry > 0) {
+      setRetryCount((prevState) => {
+        if (prevState === 0) {
+          return 0
+        } else {
+          return prevState - 1
+        }
+      })
+
+      setRetry(0)
+    }
+
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+      body: JSON.stringify({ finalInput }),
+    })
+
+    const data = await response.json()
+
+    if (response.status === 503) {
+      setRetry(data.estimated_time)
+      return
+    }
+
+    if (!response.ok) {
+      console.log(`Error: ${data.error}`)
+      setIsGenerating(false)
+      return
+    }
+
+    // Remove content from input box
+    setInput('')
+    setImg(data.image)
+    setIsGenerating(false)
+  }
+
+  const sleep = (ms) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms)
+    })
+  }
+
+  useEffect(() => {
+    const runRetry = async () => {
+      if (retryCount === 0) {
+        console.log(
+          `Model still loading after ${maxRetries} retries. Try request again in 5 minutes.`,
+        )
+        setRetryCount(maxRetries)
+        return
+      }
+
+      console.log(`Trying again in ${retry} seconds.`)
+
+      await sleep(retry * 1000)
+      await generateAction()
+    }
+
+    if (retry === 0) {
+      return
+    }
+
+    runRetry()
+  }, [retry])
+
   return (
     <div className="root">
       <Head>
-        <title>AI Avatar Generator | buildspace</title>
+        <title>Blackpink Avatar Generator | Jenny </title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>Blackpink Rose Avatar Generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2>
+              Create photos of your blackpink bias! Use "rosebp" to refer to
+              rose
+            </h2>
+          </div>
+          <div className="prompt-container">
+            <input className="prompt-box" value={input} onChange={onChange} />
+            {/* add prompt button in prompt container */}
+            <div className="prompt-buttons">
+              <a className="generate-button" onClick={generateAction}>
+                <div className="generate">
+                  <p>Create</p>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
       <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-avatar"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://twitter.com/jenstine" target="_blank" rel="noreferrer">
           <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <Image src={twitterLogo} alt="twitter logo" />
+            <p>check out my twitter!</p>
           </div>
         </a>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
